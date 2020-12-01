c880d098a64116d5e1992c9c8992dda8641f5382
Jesse Brown <brownjesse8895@yahoo.com>
2020-12-01 21:44:39 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,7 +1,6 @@
 magic_exit_number = 7
 count = 0 
-while count < 10 do 
-  break if count == magic_exit_number 
-  puts "I am the #{count}, I love to count"
-  count += 1
-end
+while count < 10 && count != magic_exit_number do
+  puts "I am the #{count},"
+  count += 1 
+end 
